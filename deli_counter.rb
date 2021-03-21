8fe6baef0c3d6f6a04267339571c27047a756cc3
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-03-21 17:09:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+
+def line(array)
+  if array.length > 0
+  array.each_index(1) {|index| puts "The line is currently: #{index}"}
+  else puts "The line is currently empty."
+  end
+end
+
+def take_a_number(array, name)
+ array.each_with_index(1).push {|name, index|puts "Welcome, #{name}. You are number #{index} in line." }
+ end
+
+ def now_serving(array)
+  if array.length > 0
+    array.each_with_index(1) {|name, index|puts "Currently serving #{name}#{index}"}
+
+  else array.length == 0
+
+ end
+end
