5885d40142f71817d7fb258d7faf2ccdb58d0480
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-03-21 17:35:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,13 +12,7 @@ def line(katz_deli)
    end
 
 
-  else
-    line_method_array.each.with_index(0) do |name, index|
-      line_method_array.push("#{index}. #{name}")
-      # push method will push the name into the index
-    end
-    puts "The line is: #{line_method_array.join(" ")}"
-  end
+  
 end
 def take_a_number(katz_deli, name)
 katz_deli.each_with_index(1).push {|name, index|puts "Welcome, #{name}. You are number #{index} in line." }
