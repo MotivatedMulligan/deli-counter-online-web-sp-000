d853b7c622c368282e154ace4ce0231a2ff5830d
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-03-21 17:39:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,18 +11,17 @@ def line(katz_deli)
      puts "The line is: #{line_method_array.join(" ")}"
    end
 
-
-
 end
 def take_a_number(katz_deli, name)
 katz_deli.push(name)
 puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
  def now_serving(array)
-  if array.length > 0
-    array.each_with_index(1) {|name, index|puts "Currently serving #{name}#{index}"}
-
-  else array.length == 0
-
+  if array.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
  end
 end
