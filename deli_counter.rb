a796043e1f4536d870eb070933b591d245a7e4c2
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-03-21 17:35:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,27 @@
 # Write your code here.
 
 def line(katz_deli)
-  if katz_deli.empty?
+  line_method_array = []
+  if katz_deli.length == 0
      puts "The line is currently empty."
+   else
+     line_method_array.each.with_index(0) do |name, index|
+       line_method_array.push("#{index}. #{name}")
+     end
+     puts "The line is: #{line_method_array.join(" ")}"
+   end
+
+
   else
-    list_of_names = "The line is currently: "
-    list_of_names = katz_deli.each.with_index(1) do |element, index|"#{index}. #{element}"end.join(" ")
-     puts "The line is currently: #{list_of_names}"
+    line_method_array.each.with_index(0) do |name, index|
+      line_method_array.push("#{index}. #{name}")
+      # push method will push the name into the index 
+    end
+    puts "The line is: #{line_method_array.join(" ")}"
   end
 end
-
-def take_a_number(array, name)
- array.each_with_index(1).push {|name, index|puts "Welcome, #{name}. You are number #{index} in line." }
+def take_a_number(katz_deli, name)
+katz_deli.each_with_index(1).push {|name, index|puts "Welcome, #{name}. You are number #{index} in line." }
  end
 
  def now_serving(array)
